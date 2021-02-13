dd62ff0717ad1928bab35e2b42c0b0e0686b20d1
Samuel Sumeracki <sj.sumeracki@gmail.com>
2021-02-13 17:40:54 -0500

    minor changes

---
+++ encyclopedia/newpage.py
@@ -0,0 +1,12 @@
+# Read documentation found here:
+# https://docs.djangoproject.com/en/3.0/topics/forms/
+
+from django import forms
+
+# Create form to make a new wiki entry
+class NewEntry(forms.Form):
+    
+    title = forms.CharField(label='Article Name', max_length=50, required=True)
+
+    content = forms.CharField(label="Write your article", required=True)
+
